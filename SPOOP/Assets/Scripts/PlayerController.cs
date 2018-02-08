ff5db6eee9d5ddac53eb64c6b82d31987910fc73
TheJosephChen <violuminescence@gmail.com>
2018-02-08 15:29:47 -0500

    PlayerController script: move/run/jump

---
+++ SPOOP/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public float height;
+	public float runSpeed;
+
+	private Rigidbody rb;
+	private float runMove;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+		runMove = speed * runSpeed;
+	}
+
+	void FixedUpdate ()
+	{
+		// regular player movement
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		// check if running
+		if (Input.GetKey (KeyCode.LeftShift)) {
+			rb.AddForce (movement * runMove);
+		} else {
+			// not running
+			rb.AddForce (movement * speed);
+		}
+
+		// jumping
+		//TODO: check if player is already jumping
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			rb.AddForce (Vector3.up * height);
+		}
+	}
+}
