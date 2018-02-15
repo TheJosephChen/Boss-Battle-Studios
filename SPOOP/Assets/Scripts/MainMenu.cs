a3aba90b350c54adbd9517180eac3b86d70672d3
Arbina Camaj <camaj.a@husky.neu.edu>
2018-02-15 18:50:14 -0500

    main menu

---
+++ SPOOP/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    public bool Start;
+    public bool Quit;
+
+    void OnMouseUp()
+    {
+        if (Start)
+        {
+            
+        }
+        if (Quit)
+        {
+            Application.Quit();
+        }
+    }
+}
