8b33bf21c0a8c8ec8ef512c8cc162d2787069517
Istiaque Shanjib <ishanjib@gmail.com>
2018-02-20 18:50:19 -0500

    tabs to spaces

--- SPOOP/Assets/Scripts/Character Creation/CharacterCreationNextLevel.cs
+++ SPOOP/Assets/Scripts/Character Creation/CharacterCreationNextLevel.cs
@@ -5,23 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class CharacterCreationNextLevel : MonoBehaviour
 {
-	public GameObject character;
+    public GameObject character;
 
     public void OnMouseButton()
     {
-		GameObject child = null;
-		for (int i = 0; i < character.transform.childCount; i++)
-		{
-			var temp = character.transform.GetChild (i);
-			if (temp.gameObject.activeSelf)
-			{
-				child = temp.gameObject;
-				child.transform.SetParent (null);
-			}
-		}
-		DontDestroyOnLoad (child);
-		child.transform.position = new Vector3 (0, 3, -20);
-		child.GetComponent<Rigidbody>().useGravity = true;
+        GameObject child = null;
+        for (int i = 0; i < character.transform.childCount; i++)
+        {
+            var temp = character.transform.GetChild (i);
+            if (temp.gameObject.activeSelf)
+            {
+                child = temp.gameObject;
+                child.transform.SetParent (null);
+            }
+        }
+        DontDestroyOnLoad (child);
+        child.transform.position = new Vector3 (0, 3, -20);
+        child.GetComponent<Rigidbody>().useGravity = true;
         SceneManager.LoadScene("Lobby");
     }
 }
