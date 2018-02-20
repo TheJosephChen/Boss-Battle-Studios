b290add0047a95f0e6a985f3538069b89f900f8b
Arbina Camaj <camaj.a@husky.neu.edu>
2018-02-20 18:12:10 -0500

    lobby to level 1 transfer

---
+++ SPOOP/Assets/Scripts/Lobby/LobbyLevel1NextLevel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LobbyLevel1NextLevel : MonoBehaviour {
+
+    void OnTriggerEnter(BoxCollider level1)
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+}
