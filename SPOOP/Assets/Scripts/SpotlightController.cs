822ebc44202f3bd5b17477f09578474468444174
TheJosephChen <violuminescence@gmail.com>
2018-02-20 19:52:54 -0500

    Lobby Spotlight

---
+++ SPOOP/Assets/Scripts/SpotlightController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// attach script to main camera
+public class SpotlightController : MonoBehaviour {
+
+	// object to follow (player)
+	public GameObject player;
+
+	private Vector3 offset;
+
+	void Start ()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player");
+		offset = transform.position - player.transform.position;
+	}
+
+	void LateUpdate ()
+	{
+		transform.position = player.transform.position + offset;
+	}
+}
