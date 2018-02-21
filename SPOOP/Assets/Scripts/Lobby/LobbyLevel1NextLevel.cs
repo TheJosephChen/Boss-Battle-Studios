0a1defa8c74d4bc5e12f3675bf0e8e4ffc82f1ec
Istiaque Shanjib <ishanjib@gmail.com>
2018-02-20 19:47:35 -0500

    adjust scripts to properly change scenes

--- SPOOP/Assets/Scripts/Lobby/LobbyLevel1NextLevel.cs
+++ SPOOP/Assets/Scripts/Lobby/LobbyLevel1NextLevel.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LobbyLevel1NextLevel : MonoBehaviour {
-
-    void OnTriggerEnter(BoxCollider level1)
+public class LobbyLevel1NextLevel : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
     {
+        DontDestroyOnLoad (other.gameObject);
         SceneManager.LoadScene("Level 1");
     }
 }
