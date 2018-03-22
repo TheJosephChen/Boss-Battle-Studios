1e402797b926e42fba23e33a9e27f3302b4a7f8a
Istiaque Shanjib <ishanjib@gmail.com>
2018-03-22 19:13:50 -0400

    button to hit and door to lobby

---
+++ SPOOP/Assets/Scripts/Level1/ButtonHit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHit : MonoBehaviour
+{
+    public GameObject door;
+
+    void OnCollisionEnter (Collision other)
+    {
+        if (other.transform.CompareTag ("Bullet"))
+            door.GetComponent<Renderer> ().material.color = Color.white;
+    }
+}
---
+++ SPOOP/Assets/Scripts/Level1/Level1ToLobby.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level1ToLobby : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        other.transform.SetParent (null);
+        other.transform.position = new Vector3 (-1, 5, -42);
+        other.GetComponent<Rigidbody> ().velocity = Vector3.zero;
+        other.GetComponent<Rigidbody> ().angularVelocity = Vector3.zero;
+        other.GetComponent<PlayerController> ().isGrounded = false;
+        DontDestroyOnLoad (other.gameObject);
+        SceneManager.LoadScene ("Lobby");
+    }
+}
