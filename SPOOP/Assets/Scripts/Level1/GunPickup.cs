04e977296eeacfbeb906d1446959b9ba4f39d5e8
Istiaque Shanjib <ishanjib@gmail.com>
2018-03-22 17:42:06 -0400

    GunPickup script

---
+++ SPOOP/Assets/Scripts/Level1/GunPickup.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunPickup : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    {
+        GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerController> ().level1Completed = true;
+        if (other.transform.CompareTag ("Player"))
+            transform.gameObject.SetActive (false);
+    }
+}
