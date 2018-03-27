207e8cfe29da92905c5f4230e72c0d4bb388f4d4
TheJosephChen <violuminescence@gmail.com>
2018-03-27 16:59:35 -0400

    Switch lvl 1 2 lights. disable lvl 1 door

--- SPOOP/Assets/Scripts/Lobby/LobbyLevel1NextLevel.cs
+++ SPOOP/Assets/Scripts/Lobby/LobbyLevel1NextLevel.cs
@@ -7,11 +7,16 @@ public class LobbyLevel1NextLevel : MonoBehaviour
 {
     void OnTriggerEnter(Collider other)
     {
-        other.transform.SetParent (null);
-        other.transform.position = new Vector3 (0, 3, 0);
-        other.GetComponent<Rigidbody> ().velocity = Vector3.zero;
-        other.GetComponent<Rigidbody> ().angularVelocity = Vector3.zero;
-        DontDestroyOnLoad (other.gameObject);
-        SceneManager.LoadScene ("Level 1");
+        if (!other.GetComponent<PlayerController> ().level1Completed) 
+        {
+            other.transform.SetParent (null);
+            other.transform.position = new Vector3 (0, 3, 0);
+            other.GetComponent<Rigidbody> ().velocity = Vector3.zero;
+            other.GetComponent<Rigidbody> ().angularVelocity = Vector3.zero;
+            DontDestroyOnLoad (other.gameObject);
+            SceneManager.LoadScene ("Level 1");
+        } else {
+            gameObject.GetComponent<BoxCollider> ().isTrigger = false;
+        }
     }
 }
