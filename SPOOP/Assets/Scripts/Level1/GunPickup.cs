41d1f08b45e7833a99b80efe15b18ca3ff5c7660
Istiaque Shanjib <ishanjib@gmail.com>
2018-03-27 16:26:15 -0400

    add popup dialog on gun pickup

--- SPOOP/Assets/Scripts/Level1/GunPickup.cs
+++ SPOOP/Assets/Scripts/Level1/GunPickup.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class GunPickup : MonoBehaviour
 {
@@ -8,6 +9,9 @@ public class GunPickup : MonoBehaviour
     {
         GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerController> ().level1Completed = true;
         if (other.transform.CompareTag ("Player"))
+        {
             transform.gameObject.SetActive (false);
+            EditorUtility.DisplayDialog ("You got a gun!", "Press enter to fire!", "Okay");
+        }
     }
 }
