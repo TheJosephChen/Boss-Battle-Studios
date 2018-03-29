3d9232324bd74eadb1d95cbd17852c493dc822c2
Istiaque Shanjib <ishanjib@gmail.com>
2018-03-29 17:39:24 -0400

    re-arrange stuff in level 2

--- SPOOP/Assets/Scripts/Level2/SwitchPickup.cs
+++ SPOOP/Assets/Scripts/Level2/SwitchPickup.cs
@@ -5,13 +5,13 @@ using UnityEditor;
 
 public class SwitchPickup : MonoBehaviour
 {
-	void OnCollisionEnter(Collision other)
-	{
-		GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerController> ().level2Completed = true;
-		if (other.transform.CompareTag ("Player"))
-		{
-			transform.gameObject.SetActive (false);
-			EditorUtility.DisplayDialog ("You got a gravity switch!", "Press <not implemented> to activate!", "Okay");
-		}
-	}
+    void OnCollisionEnter(Collision other)
+    {
+        GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerController> ().level2Completed = true;
+        if (other.transform.CompareTag ("Player"))
+        {
+            transform.gameObject.SetActive (false);
+            EditorUtility.DisplayDialog ("You got a gravity switch!", "Press <not implemented> to activate!", "Okay");
+        }
+    }
 }
