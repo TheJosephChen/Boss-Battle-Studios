bcf2b4f77aaa3f60e8b37ddf7ae7c824ad6cb043
TheJosephChen <violuminescence@gmail.com>
2018-03-29 16:48:47 -0400

    Target asset and script

---
+++ SPOOP/Assets/Scripts/TargetHitController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetHitController : MonoBehaviour
+{
+    public GameObject platform;
+    public Material white;
+    public Material red;
+    public Material green;
+
+    private bool hit;
+
+    void Start ()
+    {
+        hit = false;
+    }
+
+    void OnCollisionEnter (Collision other)
+    {
+        if (other.transform.CompareTag ("Bullet")) 
+        {
+            bool isActive = platform.gameObject.activeSelf;
+            platform.gameObject.SetActive (!isActive);
+            foreach (Renderer rend in GetComponentsInChildren<Renderer>()) 
+            {
+                if (rend.material == white)
+                    break;
+                if (!hit)
+                    rend.material = green;
+                else
+                    rend.material = red;
+            }
+            hit = !hit;
+        }   
+    }
+}
